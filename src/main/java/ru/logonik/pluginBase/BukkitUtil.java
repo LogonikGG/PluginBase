8cbc3f770d7c7296ecb6b07a82d0629edaa15999
Logonik <reglogonik@mail.ru>
2025-08-17 17:28:47 +0500

    add: giveOrDrop util method

--- src/main/java/ru/logonik/pluginBase/BukkitUtil.java
+++ src/main/java/ru/logonik/pluginBase/BukkitUtil.java
@@ -1,12 +1,24 @@
 package ru.logonik.pluginBase;
 
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 
 public class BukkitUtil {
+
+    public static void giveItemOrDrop(Player player, ItemStack... itemStacks) {
+        HashMap<Integer, ItemStack> notFit = player.getInventory().addItem(itemStacks);
+        Location location = player.getLocation();
+        for (ItemStack notFitted : notFit.values()) {
+            if(notFitted == null) continue;
+            location.getWorld().dropItem(location, notFitted);
+        }
+    }
 
     public static ItemStack[] deepCopyItemStackArray(ItemStack[] original) {
         if (original == null) {

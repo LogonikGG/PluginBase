a66032e5be4a1edfea2431818a842d7771ba3603
Logonik <reglogonik@mail.ru>
2025-10-07 15:41:21 +0500

    refactoring

--- src/main/java/ru/logonik/pluginBase/db/DatabaseConfigMapper.java
+++ src/main/java/ru/logonik/pluginBase/db/DatabaseConfigMapper.java
@@ -10,32 +10,18 @@ public class DatabaseConfigMapper {
         return loadFromSection(plugin.getConfig(), "database");
     }
 
-    public static DatabaseConfig loadFromConfig(FileConfiguration config, String basePath) {
-        DatabaseConfig dbConfig = new DatabaseConfig();
-
-        dbConfig.setDriverClass(config.getString(basePath + ".driverClass"));
-        dbConfig.setDriverJar(config.getString(basePath + ".driverJar"));
-        dbConfig.setUrl(config.getString(basePath + ".url"));
-        dbConfig.setUsername(config.getString(basePath + ".username"));
-        dbConfig.setPassword(config.getString(basePath + ".password"));
-
-        return dbConfig;
+    public static DatabaseConfig loadFromSection(FileConfiguration config, String sectionName) {
+        return loadFromSection(config.getConfigurationSection(sectionName));
     }
 
-    public static DatabaseConfig loadFromSection(FileConfiguration config, String sectionName) {
-        ConfigurationSection section = config.getConfigurationSection(sectionName);
-        if (section != null) {
-            DatabaseConfig dbConfig = new DatabaseConfig();
-
-            dbConfig.setDriverClass(section.getString("driverClass"));
-            dbConfig.setDriverJar(section.getString("driverJar"));
-            dbConfig.setUrl(section.getString("url"));
-            dbConfig.setUsername(section.getString("username"));
-            dbConfig.setPassword(section.getString("password"));
-
-            return dbConfig;
-        } else {
-            return loadFromConfig(config, sectionName);
-        }
+    private static DatabaseConfig loadFromSection(ConfigurationSection section) {
+        if (section == null) return null;
+        DatabaseConfig dbConfig = new DatabaseConfig();
+        dbConfig.setDriverClass(section.getString("driverClass"));
+        dbConfig.setDriverJar(section.getString("driverJar"));
+        dbConfig.setUrl(section.getString("url"));
+        dbConfig.setUsername(section.getString("username"));
+        dbConfig.setPassword(section.getString("password"));
+        return dbConfig;
     }
 }

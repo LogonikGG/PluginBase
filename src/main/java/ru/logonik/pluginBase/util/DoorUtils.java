e5813589e65626e033a5266aecc01b8756b096d8
Logonik <95104347+LogonikGG@users.noreply.github.com>
2026-01-15 14:49:31 +0500

    ref: DoorUtils

--- src/main/java/ru/logonik/pluginBase/util/DoorUtils.java
+++ src/main/java/ru/logonik/pluginBase/util/DoorUtils.java
@@ -12,61 +12,64 @@ public final class DoorUtils {
     private DoorUtils() {
     }
 
-    public static boolean openDoor(Location location) {
-        return setDoorOpen(location, true);
+    public static boolean openDoor(Block block) {
+        return setDoorOpen(block, true);
     }
 
-    public static boolean closeDoor(Location location) {
-        return setDoorOpen(location, false);
+    public static boolean closeDoor(Block block) {
+        return setDoorOpen(block, false);
     }
 
-    public static boolean toggleDoor(Location location) {
-        Block block = location.getBlock();
-        Block doorBlock = getDoorBlock(block);
+    public static boolean toggleDoor(Block block) {
+        Block doorBlock = getBottomDoorBlock(block);
         if (doorBlock == null) return false;
 
         Door door = (Door) doorBlock.getBlockData();
         return setDoorState(doorBlock, !door.isOpen());
     }
 
-    private static boolean setDoorOpen(Location location, boolean open) {
-        Block block = location.getBlock();
-        Block doorBlock = getDoorBlock(block);
+    public static boolean openDoor(Location location) {
+        return openDoor(location.getBlock());
+    }
+
+    public static boolean closeDoor(Location location) {
+        return closeDoor(location.getBlock());
+    }
+
+    public static boolean toggleDoor(Location location) {
+        return toggleDoor(location.getBlock());
+    }
+
+    private static boolean setDoorOpen(Block block, boolean open) {
+        Block doorBlock = getBottomDoorBlock(block);
         if (doorBlock == null) return false;
 
         return setDoorState(doorBlock, open);
     }
 
-    private static boolean setDoorState(Block doorBlock, boolean open) {
-        BlockData data = doorBlock.getBlockData();
+    private static boolean setDoorState(Block bottomBlock, boolean open) {
+        BlockData data = bottomBlock.getBlockData();
         if (!(data instanceof Door)) return false;
         Door door = (Door) data;
 
         if (door.isOpen() == open) return true;
 
         door.setOpen(open);
-        doorBlock.setBlockData(door, true);
+        bottomBlock.setBlockData(door, true);
 
-        Block otherHalf = getOtherHalf(doorBlock, door);
-        if (otherHalf != null) {
-            otherHalf.setBlockData(door, true);
-        }
+        Block topBlock = bottomBlock.getRelative(BlockFace.UP);
+        topBlock.setBlockData(door, true);
 
         return true;
     }
 
-    private static Block getDoorBlock(Block block) {
-        if (!(block.getBlockData() instanceof Door)) return null;
-        Door door = (Door) block.getBlockData();
+    private static Block getBottomDoorBlock(Block block) {
+        BlockData data = block.getBlockData();
+        if (!(data instanceof Door)) return null;
+        Door door = (Door) data;
 
         return door.getHalf() == Bisected.Half.TOP
                 ? block.getRelative(BlockFace.DOWN)
                 : block;
-    }
-
-    private static Block getOtherHalf(Block block, Door door) {
-        return door.getHalf() == Bisected.Half.BOTTOM
-                ? block.getRelative(BlockFace.UP)
-                : block.getRelative(BlockFace.DOWN);
     }
 }

647b19c40b6c7fc882597373966c7c92b4d0baae
Logonik <95104347+LogonikGG@users.noreply.github.com>
2026-03-22 16:46:19 +0500

    fix (Coordinates)

--- src/main/java/ru/logonik/pluginBase/models/Coordinates.java
+++ src/main/java/ru/logonik/pluginBase/models/Coordinates.java
@@ -1,21 +1,35 @@
 package ru.logonik.pluginBase.models;
 
+import org.bukkit.Location;
+
 public class Coordinates {
     private double x;
     private double y;
     private double z;
-    private float pitch;
     private float yaw;
+    private float pitch;
 
     public Coordinates() {
     }
 
-    public Coordinates(double x, double y, double z, float pitch, float yaw) {
+    /**
+     * @deprecated yaw and pitch have changed positions
+     */
+    @Deprecated()
+    public Coordinates(double x, double y, double z, float yaw, float pitch) {
         this.x = x;
         this.y = y;
         this.z = z;
-        this.pitch = pitch;
         this.yaw = yaw;
+        this.pitch = pitch;
+    }
+
+    public Coordinates(Location location) {
+        this.x = location.getX();
+        this.y = location.getY();
+        this.z = location.getZ();
+        this.pitch = location.getPitch();
+        this.yaw = location.getYaw();
     }
 
     public double getX() {
